2449d1e458cbfdaba0f0b0044d0671bc3d2c85a7
Yin Kokpheng <yinkokpheng@yahoo.com>
2015-11-27 14:12:34 +0700

    Initial Commit

---
+++ WCProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WCProject
+//
+//  Created by Yin Kokpheng on 11/27/15.
+//  Copyright © 2015 Yin Kokpheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
